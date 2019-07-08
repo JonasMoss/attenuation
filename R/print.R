85c8ebb320b524aa799bd50884db3542f8148187
Jonas Moss <jonas.gjertsen@gmail.com>
2019-07-08 21:22:54 +0200

    :bug: Fixed small bug.

--- R/print.R
+++ R/print.R
@@ -21,7 +21,7 @@ print.ccaf = function(x, digits = 3, ...) {
   cat("Arguments\n")
   cat("    Sample correlations:", signif(r[1], digits),
                                   signif(r[2], digits),
-                                  signif(r[1], digits))
+                                  signif(r[3], digits))
   cat("\n    Sample sizes:", N[1], N[2], N[3])
 
   if(length(x) == 1) {
